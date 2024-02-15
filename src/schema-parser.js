793351dac37fb4dabcab4550fc9a57f3238332e0
03-CiprianoG <ciprianogi03@gmail.com>
2024-02-15 08:15:46 +0100

    Add conditional check withing schema-parser for null values

--- src/schema-parser.js
+++ src/schema-parser.js
@@ -8,32 +8,40 @@ class SchemaParser {
     this.schema = schema;
   }
 
-  /**
-   * Extracts foreign keys from the schema
-   *
-   * @returns Object
-   */
   getForeignKeys() {
     let foreignKeys = {};
 
-    Object.keys(this.schema).forEach((table) => {
-      if (this.schema[table] === null) return;
+    for (let table of Object.keys(this.schema)) {
+      // Continue if the schema entry is null or not a string
+      if (!this.schema[table] || typeof this.schema[table] !== "string") {
+        continue;
+      }
 
+      console.log("about to process: ", this.schema[table]);
       let indexes = this.schema[table].split(",");
 
       foreignKeys[table] = indexes
         .filter((idx) => idx.indexOf("->") !== -1)
         .map((idx) => {
-          // split the column and foreign table info
+          // Split the column and foreign table info
           let [column, target] = idx.split("->").map((x) => x.trim());
+
+          // Additional check to ensure 'target' is not null/undefined and contains a '.'
+          if (!target || target.indexOf(".") === -1) {
+            console.error(
+              `Invalid target format in schema for table '${table}': '${idx}'`
+            );
+            return null; // Return null or some other way to handle this error condition
+          }
 
           return {
             index: column,
             targetTable: target.split(".")[0],
             targetIndex: target.split(".")[1],
           };
-        });
-    });
+        })
+        .filter(Boolean); // Remove any null values resulting from invalid target formats
+    }
 
     return foreignKeys;
   }
@@ -46,12 +54,16 @@ class SchemaParser {
   getCleanedSchema() {
     let schema = {};
 
-    Object.keys(this.schema).forEach((table) => {
+    for (let table of Object.keys(this.schema)) {
+      if (!this.schema[table] || typeof this.schema[table] !== "string") {
+        continue;
+      }
+
       let indexes = this.schema[table].split(",");
 
       // Remove foreign keys syntax before calling the original method
       schema[table] = indexes.map((idx) => idx.split("->")[0].trim()).join(",");
-    });
+    }
 
     return schema;
   }
